ab47564151b25e8de81fa0e48175e2a44b7c9611
Matt Denwood <md@sund.ku.dk>
2025-02-27 06:24:29 +0100

    Update R6utils.R

--- R/R6utils.R
+++ R/R6utils.R
@@ -5,19 +5,30 @@
       !name %in% c(names(lhs),"active","private","super","deep_clone") &&
       !str_sub(name, 1L, 1L)==".")
     stop(name, " not present in environment")
-  do.call(base::`$`, args=list(lhs, name))
+  do.call(base::`$`, elem=list(lhs, name))
 }
 
 mlist <- function(..., .dummy=NULL){
-  args <- list(...)
-  if(any(names(args)=="")){
-    an <- names(args)
-    an[names(args)==""] <- "MISSING"
+  elem <- list(...)
+  if(any(names(elem)=="")){
+    an <- names(elem)
+    an[names(elem)==""] <- "MISSING"
     stop("Empty names in member/method list: ", paste0(an,collapse=", "))
   }
-  if(any(table(names(args))>1)){
-    tn <- table(names(args))
+  if(any(table(names(elem))>1)){
+    tn <- table(names(elem))
     stop("Duplicated names in member/method list: ", paste0(names(tn)[tn>1], collapse=", "))
   }
-  do.call(list, args)
+
+  if(FALSE){
+    for(i in which(sapply(elem, is.function))){
+      args <- names(formals(elem[[i]]))
+      bod <- as.character(body(elem[[i]]))
+      if(length(bod)==2L && is.character(bod[2])){
+        stop("Dispatch to function called bod[2] with arguments args")
+      }
+    }
+  }
+
+  do.call(list, elem)
 }
