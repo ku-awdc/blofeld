efeedc182455085d9d7a43cfabf412b135dca96c
Matt Denwood <md@sund.ku.dk>
2025-11-12 08:05:53 +0100

    Create Compartment.h

---
+++ inst/include/blofeld/Compartment.h
@@ -0,0 +1,259 @@
+#ifndef BLOFELD_COMPARTMENT_H
+#define BLOFELD_COMPARTMENT_H
+
+#include <array>
+
+// 
+
+// TODO: should take/carry be immediate?  So only insert is done on delay?
+// TODO: allow s_ncomps = 0 in which case it is set at run time
+
+namespace blofeld
+{
+  enum class ModelType
+  {
+    deterministic,  // Uses double and simple maths
+    stochastic      // Uses int and random sampling
+  };
+
+  enum class CompCont
+  {
+    disabled,       // Removed - compiles to nothing
+    array,          // Fixed size (including 1, but not zero)
+    inplace_vector, // Emulation of c++26 inplace_vector i.e. stack-based, dynamic up to max size
+    vector,         // Heap-based, dynamic - s_ncomps is ignored
+    balancing       // Fixed size of 1 and allows negative values (for birth/death) - s_ncomps is ignored
+  };
+
+  // Literal type
+  class CompType
+  {
+    const CompCont m_compcont;
+    const size_t m_n;
+    
+  public:
+    CompType(CompCont container) :
+      m_compcont(container), m_n(0)
+    {
+      
+    }
+
+    CompType(CompCont const container, size_t const n) :
+      m_compcont(container), m_n(n)
+    {
+      
+    }    
+  };
+  
+  template <auto s_cts, ModelType s_mtype, CompType s_ctype>
+  class Compartment
+  {
+  private:
+    // TODO: these need to be array or vector, depending on s_ncomps==0
+    std::array<double, s_ncomps> m_values { };
+    std::array<double, s_ncomps> m_changes { };
+  
+    const int m_ncomps;
+
+    auto check_compartments() const noexcept(!s_cts.debug)
+      -> void
+    {
+      if constexpr (!s_cts.debug && s_ncomps!=0U)
+      {
+        if (m_ncomps != s_ncomps) Rcpp::stop("Non-matching number of sub-compartments");
+      }
+      if constexpr (s_ncomps <= 0U)
+      {
+        Rcpp::stop("Negative s_ncomps are not allowed, and values of 0 are not yet supported");
+      }    
+    }
+
+    Compartment() = delete;
+  
+  public:
+    explicit Compartment(int const ncomps) noexcept(!s_cts.debug)
+      : m_ncomps(ncomps)
+    {
+      check_compartments();
+    }
+
+    Compartment(int const ncomps, double const value) noexcept(!s_cts.debug)
+      : m_ncomps(ncomps)
+    {
+      set_sum(value);
+      check_compartments();
+    }
+
+    auto apply_changes()
+      noexcept(!s_cts.debug)
+      -> void
+    {
+      for(int i=0; i<s_ncomps; ++i)
+      {
+        m_values[i] += m_changes[i];
+        m_changes[i] = 0.0;
+      
+        // Zap occasional small negative values (no adjustment to m_Z, so it can't happen too often):
+        if(m_values[i] < 0.0 && std::abs(m_values[i]) < s_cts.tol){        
+          m_values[i] = 0.0;
+        }
+      
+        if constexpr (s_cts.debug){
+          if(m_values[i] < 0.0){
+            Rcpp::Rcout << m_values[i] << " < 0.0\n";
+            Rcpp::stop("Applying changes caused a negative value");
+          }
+        }      
+      }
+    }
+
+    [[nodiscard]] auto get_sum()
+      const noexcept(!s_cts.debug)
+      -> double
+    {
+      double const rv = std::accumulate(m_values.begin(), m_values.end(), 0.0);
+      return rv;
+    }
+
+    auto set_sum(double const value) noexcept(!s_cts.debug)
+      -> void
+    {
+      // TOODO: switch to distribute balanced or all in first box
+      for(auto& val : m_values){
+        // val = value / static_cast<double>(s_ncomps);
+        val = 0.0;
+      }
+      m_values[0] = value;
+    }
+
+    /*
+    auto set_rate(double const rate, std::array<double, s_ndests> const& proportions,
+                  std::array<double, s_ndests> const& proportions_final)
+      noexcept(!s_cts.debug)
+      -> void
+    {
+
+    }
+
+    auto set_rate(double const rate, std::array<double, s_ndests> const& proportions) noexcept(!s_cts.debug)
+      -> void
+    {
+      set_rate(rate, proportions, proportions);
+    }
+    */
+
+    [[nodiscard]] auto take_rate(double const rate, double const d_time) noexcept(!s_cts.debug)
+      -> double
+    {
+      double const prop = 1.0 - std::exp(-rate * s_ncomps * d_time);
+      double const rv = take_prop(prop);
+      return rv;
+    }
+
+    /*
+    auto remove_number(double const number) noexcept(!s_cts.debug)
+      -> double
+    {
+      if (number > 0.0)
+      {
+        double const propr = 1.0 - (number / get_sum());
+        if constexpr (s_cts.debug)
+        {
+          if (propr > 1.0) Rcpp::stop("Negative number supplied to remove from compartment");
+          if (propr < 0.0) Rcpp::stop("Attempt to remove too high a number from compartment");
+        }
+        for(auto& val : m_values){
+          val *= propr;
+        }
+      }
+      return number;
+    }
+    */
+
+    [[nodiscard]] auto take_prop(double const prop) noexcept(!s_cts.debug)
+      -> double
+    {
+      double rv = 0.0;
+      for(int i=0; i<s_ncomps; ++i){
+        double const tt = m_values[i] * prop;
+        rv += tt;
+        m_changes[i] -= tt;
+      }
+      if constexpr (s_cts.debug){
+        if(rv < 0.0) Rcpp::stop("Returning negative value from take_prop");
+      }
+      return rv;
+    }
+
+    [[nodiscard]] auto carry_rate(double const rate, double const d_time)
+      noexcept(!s_cts.debug)
+      -> double
+    {
+      double const prop = 1.0 - std::exp(-rate * s_ncomps * d_time);
+      double carry = 0.0;
+      for(int i=0; i<s_ncomps; ++i){
+        m_changes[i] += carry;
+        carry = m_values[i] * prop;
+        m_changes[i] -= carry;
+      }
+      if constexpr (s_cts.debug){
+        if(carry < 0.0) Rcpp::stop("Returning negative value from carry_rate");
+      }    
+      return carry;
+    }
+
+    auto insert_value_start(double const value)
+      noexcept(!s_cts.debug)
+      -> void
+    {
+      m_changes[0] += value;
+    }
+
+    auto ptr()
+      noexcept(!s_cts.debug)
+      -> std::array<double, s_ncomps>&
+    {
+      return m_values;
+    }
+
+    auto ptr()
+      const noexcept(!s_cts.debug)
+      -> std::array<double, s_ncomps> const&
+    {
+      return m_values;
+    }
+
+    // Note: unusual + overloads return double
+    [[nodiscard]] auto operator+(double const sum)
+      const noexcept(!s_cts.debug)
+        -> double
+    {
+      return sum + get_sum();
+    }
+    template <auto sc, unsigned int ss>//, size_t sn>
+    [[nodiscard]] auto operator+(Compartment<sc, ss> const& obj)
+      const noexcept(!s_cts.debug)
+        -> double
+    {
+      return obj.get_sum() + get_sum();
+    }
+
+  };
+
+  template <auto sc, unsigned int ss>//, size_t sn>
+  [[nodiscard]] auto operator+(double const sum, Compartment<sc, ss> const& obj)
+    -> double
+  {
+    return sum + obj.get_sum();
+  }
+
+  template <auto sc, unsigned int ss>//, size_t sn>
+  [[nodiscard]] auto operator+(Compartment<sc, ss> const& obj, double const sum)
+    -> double
+  {
+    return sum + obj.get_sum();
+  }
+
+} //blofeld
+
+#endif // BLOFELD_COMPARTMENT_H
